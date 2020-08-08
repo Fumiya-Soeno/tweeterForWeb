1f1a787bbf8f0c71777b1f0a4cd41c3d298f106b
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-08-08 11:02:12 +0900

    createメソッドの定義

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -1,2 +1,6 @@
 class Api::TweetsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+  def create
+    puts Tweet.new(params[:tweet].permit(:text)).valid?
+  end
 end
